b957920a216b7eaa608f0ee14b2167eca7fe3011
Tanvi Padmawar <tanvipadmawar@gmail.com>
2026-08-03 19:31:34 +0530

    Day 14: Added budget dashboard and category-wise expense summary

--- app/src/main/java/com/example/ai_travel_planner/database/ExpenseDao.java
+++ app/src/main/java/com/example/ai_travel_planner/database/ExpenseDao.java
@@ -12,6 +12,7 @@ public interface ExpenseDao {
 
     @Insert
     void insertExpense(Expense expense);
+
     @Delete
     void deleteExpense(Expense expense);
 
@@ -23,5 +24,25 @@ public interface ExpenseDao {
 
     @Query("SELECT COUNT(*) FROM expense_table")
     int getExpenseCount();
+
+    // Category-wise Expense Summary
+
+    @Query("SELECT SUM(amount) FROM expense_table WHERE category='Food'")
+    double getFoodExpense();
+
+    @Query("SELECT SUM(amount) FROM expense_table WHERE category='Hotel'")
+    double getHotelExpense();
+
+    @Query("SELECT SUM(amount) FROM expense_table WHERE category='Transport'")
+    double getTransportExpense();
+
+    @Query("SELECT SUM(amount) FROM expense_table WHERE category='Shopping'")
+    double getShoppingExpense();
+
+    @Query("SELECT SUM(amount) FROM expense_table WHERE category='Ticket'")
+    double getTicketExpense();
+
+    @Query("SELECT SUM(amount) FROM expense_table WHERE category='Other'")
+    double getOtherExpense();
 
 }
--- app/src/main/java/com/example/ai_travel_planner/history/ExpenseHistoryActivity.java
+++ app/src/main/java/com/example/ai_travel_planner/history/ExpenseHistoryActivity.java
@@ -21,6 +21,14 @@ public class ExpenseHistoryActivity extends AppCompatActivity {
     TextView tvRemainingBudget;
     TextView tvProgress;
     ProgressBar progressBudget;
+    TextView tvBudgetStatus;
+    TextView tvBudget;
+    TextView tvFood;
+    TextView tvHotel;
+    TextView tvTransport;
+    TextView tvShopping;
+    TextView tvTicket;
+    TextView tvOther;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,6 +41,15 @@ public class ExpenseHistoryActivity extends AppCompatActivity {
         tvRemainingBudget = findViewById(R.id.tvRemainingBudget);
         tvProgress = findViewById(R.id.tvProgress);
         progressBudget = findViewById(R.id.progressBudget);
+        tvBudget = findViewById(R.id.tvBudget);
+        tvBudgetStatus = findViewById(R.id.tvBudgetStatus);
+        tvFood = findViewById(R.id.tvFood);
+        tvHotel = findViewById(R.id.tvHotel);
+        tvTransport = findViewById(R.id.tvTransport);
+        tvShopping = findViewById(R.id.tvShopping);
+        tvTicket = findViewById(R.id.tvTicket);
+        tvOther = findViewById(R.id.tvOther);
+        tvOther = findViewById(R.id.tvOther);
 
         recyclerExpense.setLayoutManager(
                 new LinearLayoutManager(this)
@@ -57,7 +74,10 @@ public class ExpenseHistoryActivity extends AppCompatActivity {
         recyclerExpense.setAdapter(adapter);
 
         updateBudgetSummary();
+        updateCategorySummary();
+
     }
+
 
     private void updateBudgetSummary() {
 
@@ -100,9 +120,58 @@ public class ExpenseHistoryActivity extends AppCompatActivity {
         }
 
         progressBudget.setProgress(percentage);
+        if (remaining > totalBudget * 0.50) {
+
+            tvBudgetStatus.setText("🟢 Excellent Budget");
+
+        }
+        else if (remaining > totalBudget * 0.20) {
+
+            tvBudgetStatus.setText("🟡 Good Budget");
+
+        }
+        else if (remaining > 0) {
+
+            tvBudgetStatus.setText("🟠 Low Budget");
+
+        }
+        else {
+
+            tvBudgetStatus.setText("🔴 Over Budget");
+
+        }
 
         tvProgress.setText(
                 percentage + "% Budget Used"
         );
+    }
+    private void updateCategorySummary() {
+
+        TripDatabase db = TripDatabase.getInstance(this);
+
+        tvFood.setText(
+                "🍔 Food : ₹" +
+                        db.expenseDao().getFoodExpense());
+
+        tvHotel.setText(
+                "🏨 Hotel : ₹" +
+                        db.expenseDao().getHotelExpense());
+
+        tvTransport.setText(
+                "🚕 Transport : ₹" +
+                        db.expenseDao().getTransportExpense());
+
+        tvShopping.setText(
+                "🛍 Shopping : ₹" +
+                        db.expenseDao().getShoppingExpense());
+
+        tvTicket.setText(
+                "🎫 Ticket : ₹" +
+                        db.expenseDao().getTicketExpense());
+
+        tvOther.setText(
+                "📦 Other : ₹" +
+                        db.expenseDao().getOtherExpense());
+
     }
 }
